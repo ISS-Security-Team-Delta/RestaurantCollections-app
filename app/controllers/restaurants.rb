64f64a9ccbd2c6201d734e6c70dc5546d96f3a3b
tara-thomas <t.tarathomas@gmail.com>
2021-06-10 02:50:46 +0800

    Added dry validation checks

--- app/controllers/restaurants.rb
+++ app/controllers/restaurants.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 require 'roda'
+require_relative '../forms/new_restaurant'
 
 module RestaurantCollections
   # Web controller for Credence API
   class App < Roda
     route('restaurants') do |routing|
       routing.on do
+        puts "entered the routing for restaurants"
         # GET /restaurants/
+        @addRestaurant_route = '/restaurants/add'
+          routing.is 'add' do
+          routing.get do
+              view :add_restaurant
+          end
+        end
+
+        @restaurants_route = '/restaurants/'
         routing.get do
           if @current_account.logged_in?
             restaurant_list = GetAllRestaurants.new(App.config).call(@current_account)
@@ -19,6 +29,26 @@ module RestaurantCollections
           else
             routing.redirect '/auth/login'
           end
+        end
+
+        routing.post do
+          routing.redirect '/auth/login' unless @current_account.logged_in?
+          puts "REST: #{routing.params}"
+          restaurant_data = Form::NewRestaurant.new.call(routing.params)
+          if restaurant_data.failure?
+            flash[:error] = Form.message_values(restaurant_data)
+            routing.halt
+          end
+          puts "Calling restaurant service"
+          CreateRestaurant.new(App.config).call(
+            current_account: @current_account,
+            restaurant_data: restaurant_data.to_h
+          )
+        rescue StandardError => e
+          puts "FAILURE Creating Project: #{e.inspect}"
+          flash[:error] = 'Could not create project'
+        ensure
+          routing.redirect @restaurants_route
         end
       end
     end
---
+++ app/services/create_restaurant.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'http'
+
+module RestaurantCollections
+  # Returns an authenticated user, or nil
+  class CreateRestaurant
+    # Error for accounts that cannot be created
+    class InvalidRestaurant < StandardError
+      def message
+        'This restaurant can no longer be created: please start again'
+      end
+    end
+
+    def initialize(config)
+      @config = config
+    end
+
+    def call(current_account:, restaurant_data:)
+        puts "Entered restaurant service with data: #{restaurant_data}"
+      message = { name: restaurant_data[:name],
+                    website: restaurant_data[:repo_url],
+                    address: restaurant_data[:rest_add],
+                    menu: restaurant_data[:rest_menu]
+                }
+
+      response = HTTP.post(
+        "#{@config.API_URL}/restaurants/",
+        json: message
+      )
+
+      puts "Response: #{response.body}"
+
+      raise InvalidRestaurant unless response.code == 201
+    end
+  end
+end
